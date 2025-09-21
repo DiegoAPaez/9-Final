c9d3b5d68d55bd873286e4fa7d7033e0cab9b869
DiegoAPaez <diego.p31@hotmail.com>
2025-09-21 14:34:34 +0200

    Updated mapping paths to follow best practices

--- backend/src/main/java/com/spring/restaurantmanagementsystem/controller/OrderStateController.java
+++ backend/src/main/java/com/spring/restaurantmanagementsystem/controller/OrderStateController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/order_states")
+@RequestMapping("/api/order-states")
 @CrossOrigin("*")
 public class OrderStateController {
     private final OrderStateService orderStateService;
