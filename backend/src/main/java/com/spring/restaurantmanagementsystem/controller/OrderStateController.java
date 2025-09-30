5a0de8b4a62a782c94795819ff7de347440afdc6
DiegoAPaez <diego.p31@hotmail.com>
2025-09-30 11:38:25 +0200

    Removed unused import statements

--- backend/src/main/java/com/spring/restaurantmanagementsystem/controller/OrderStateController.java
+++ backend/src/main/java/com/spring/restaurantmanagementsystem/controller/OrderStateController.java
@@ -2,7 +2,6 @@ package com.spring.restaurantmanagementsystem.controller;
 
 import com.spring.restaurantmanagementsystem.dto.OrderStateDto;
 import com.spring.restaurantmanagementsystem.service.OrderStateService;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
